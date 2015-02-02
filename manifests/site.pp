560ada62748ac88b3f808511da02a7dcc403cdcb
Anthony Shorter <anthshor@gmail.com>
2015-02-02 20:08:44 +1300

    oracle7 and 12c_preinstall

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,14 @@
-package {"oracle-rdbms-server-11gR2-preinstall":
+package {"oracle-rdbms-server-12cR1-preinstall":
   ensure  => installed,
+  allow_virtual => false,
+}
+
+package {"openssh":
+  ensure => installed,
+  allow_virtual => false,
 }
 
 exec {"/bin/bash /vagrant/grid_user.sh":
   unless => "/bin/grep -q grid /etc/passwd",
-  require => Package["oracle-rdbms-server-11gR2-preinstall"],
+  require => Package["oracle-rdbms-server-12cR1-preinstall"],
 }
