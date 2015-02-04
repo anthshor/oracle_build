0ee56cfad673d10125d7368db65494ad4032a1da
Anthony Shorter <anthshor@gmail.com>
2015-02-04 16:47:51 +1300

    glibc

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,3 +12,8 @@ exec {"/bin/bash /vagrant/grid_user.sh":
   unless => "/bin/grep -q grid /etc/passwd",
   require => Package["oracle-rdbms-server-12cR1-preinstall"],
 }
+
+package {"glibc":
+  ensure => latest,
+  allow_virtual => false,
+}
