8407b89725e254080a9f463a98819e34b16737e0
Anthony Shorter <anthshor@gmail.com>
2015-03-24 22:55:38 +1300

    take 2

--- provision.sh
+++ provision.sh
@@ -1,13 +1,13 @@
 # Install software
-##yum install -y oracle-rdbms-server-12cR1-preinstall
-##yum install -y openssh
-##yum install -y glibc
+yum install -y oracle-rdbms-server-12cR1-preinstall
+yum install -y openssh
+yum install -y glibc
 
-##su - oracle -c 'unzip /vagrant/software/linuxamd64_12c_database_1of2.zip'
-##su - oracle -c 'unzip /vagrant/software/linuxamd64_12c_database_2of2.zip'
+su - oracle -c 'unzip /vagrant/software/linuxamd64_12c_database_1of2.zip'
+su - oracle -c 'unzip /vagrant/software/linuxamd64_12c_database_2of2.zip'
 
-##mv /etc/hosts /etc/hosts.original
-##cp /vagrant/hosts /etc/hosts
+mv /etc/hosts /etc/hosts.original
+cp /vagrant/hosts /etc/hosts
 mkdir -p /u01/app/oracle
 mkdir -p /u01/app/oraInventory
 mkdir -p /u01/oradata
@@ -20,7 +20,7 @@ chown oracle:oinstall /u01/app/oraInventory
 su - oracle -c 'mkdir -p /u01/app/oracle/product/12c'
 chmod -R 775 /u01/app
 
-##su - oracle -c '/home/oracle/database/runInstaller -silent -showProgress -promptForPassword -waitforcompletion -responseFile /vagrant/db_install.rsp'
+su - oracle -c '/home/oracle/database/runInstaller -silent -showProgress -promptForPassword -waitforcompletion -responseFile /vagrant/db_install.rsp'
 
 su - oracle -c '/u01/app/oraInventory/orainstRoot.sh'
 su - oracle -c '/u01/app/oracle/product/12c/root.sh'
