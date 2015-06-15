545d831744f2eeeba06b8321f4e4d47c1400a6bc
Anthony <anthshor@gmail.com>
2015-06-15 14:55:14 +1200

    Update box and provision.sh

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,7 @@
 # vi: set ft=ruby :
 
 Vagrant.configure(2) do |config|
-  config.vm.box = "kikitux/oracle65-1disk"
+  config.vm.box = "anthshor/OracleLinux66"
   config.vm.synced_folder "~/Dropbox/Hashicorp/Vagrant/software", "/u01/software", create: true
   config.vm.network :private_network, type: "dhcp"
   config.vm.provision "shell",  path: "provision.sh"
--- provision.sh
+++ provision.sh
@@ -1,6 +1,11 @@
 # v0.1 Initial
 # v0.2 Adding conditions to each statement for idempotence.
 # v0.3 Added proxy 
+# v0.4 sudo -u oracle -E so it inherits variables from parent process. 
+#      Alvaros code to configure /dev/shm to 95% of VMs memory. 
+#      variables ORACLE_HOME,STAGE, BASEDIR
+#      unzip -n for software instead of if statement
+#      distill down, remove uneccessary code - combine similar tests
 
 # Proxy
 [ -f /vagrant/proxy.env ] && source /vagrant/proxy.env
@@ -12,24 +17,19 @@
 # thanks Alvaro https://github.com/kikitux/stagefiles/blob/master/db/preinstall_crs_db.sh#L1
  
 echo "installing oracle-rdbms-server-12cR1-preinstall" 
-PACKAGES="oracle-rdbms-server-12cR1-preinstall openssh glibc git" 
-
+PACKAGES="oracle-rdbms-server-12cR1-preinstall" 
 rpm -q $PACKAGES 
 if [ $? -ne 0 ]; then 
-  yum clean all 
+  #yum clean all 
   yum -y install $PACKAGES  
 fi
 
-
-# Unpack previously downloaded software
-
-if [ -d /home/oracle/database ]; then
-    echo "Skipping unzipping..."
-  else
-    su - oracle -c 'unzip /u01/software/linuxamd64_12c_database_1of2.zip'
-    su - oracle -c 'unzip /u01/software/linuxamd64_12c_database_2of2.zip'
-fi
-
+#per Oracle manual, we require add rw,exec to /dev/shm
+#and will adjust size to 95% of ram
+cp /etc/fstab /etc/fstab.ori
+egrep -v '/dev/shm' /etc/fstab.ori > /etc/fstab
+awk '{ $1="MemTotal:" };END{printf "tmpfs  /dev/shm  tmpfs rw,exec,size=%.0fm,defaults  0 0\n", $2/1024*0.95}' /proc/meminfo >> /etc/fstab
+mountpoint /dev/shm 2>&1 >/dev/null  && mount -o remount,rw,exec /dev/shm || mount /dev/shm
 
 # Add hostname to hosts file. 
 # The is to avoid 
@@ -53,59 +53,60 @@ else
     fi
 fi
 
-
 # Create ORACLE_BASE directory
-
 [ -d /u01/app/oracle ] || mkdir -p /u01/app/oracle
 
+# Create stage directory
+[ -d /u01/stage ] || mkdir -p /u01/stage
 
 # Create Oracle Inventory directory
-
 [ -d /u01/app/oraInventory ] || mkdir -p /u01/app/oraInventory
 
-
 # Create database directories
-
 [ -d /u01/oradata ] || mkdir -p /u01/oradata
-[ -d /u01/recovery ] || mkdir -p /u01/recovery
-
+[ -d /u01/recovery ] || mkdir -p /u01/orafra
 
 # Change ownership to oracle:oinstall
-
-chown oracle:oinstall /u01/oradata
-chown oracle:oinstall /u01/recovery
 chown oracle:oinstall /u01/app/oracle
+chown oracle:oinstall /u01/stage
 chown oracle:oinstall /u01/app/oraInventory
+chown oracle:oinstall /u01/oradata
+chown oracle:oinstall /u01/orafra
 
+# use sudo over su
+# sudo -u oracle -E (command 1 ; command 2)
+
+#define the global variables
+export ORACLE_HOME="/u01/app/oracle/product/12c"
+export STAGE="/u01/stage"
+export BASEDIR="/vagrant"
+
+# Unpack previously downloaded software
+sudo -u oracle -E unzip -n -d $STAGE /u01/software/linuxamd64_12102_database_1of2.zip
+sudo -u oracle -E unzip -n -d $STAGE /u01/software/linuxamd64_12102_database_2of2.zip
 
 # Create directory for database software
+[ -d $ORACLE_HOME ] || sudo -u oracle -E mkdir -p $ORACLE_HOME
 
-su - oracle -c '[ -d /u01/app/oracle/product/12c ] || mkdir -p /u01/app/oracle/product/12c'
 
-
-# Set base permissions for Oracle directories
-
-[ `du -s /u01/app/oracle/product/12c | cut -f 1` -gt 1000000 ] || chmod -R 775 /u01/app
-
+if [ ! -f $ORACLE_HOME/root.sh ]; then
+  # Set base permissions for Oracle directories
+  chmod -R 775 /u01/app
 
 # Run Oracle installer in silent mode with a response file
 # Referred to Oracle Database Install Guide on Linux, Appendix A
 # http://docs.oracle.com/database/121/LADBI/app_nonint.htm#LADBI7838
 # Edited db_install.rsp with valid inputs (depends on requirements)
-
-su - oracle -c '[ -e /u01/app/oracle/product/12c/root.sh ] || /home/oracle/database/runInstaller -silent -showProgress -promptForPassword -waitforcompletion -responseFile /vagrant/db_install.rsp'
-
+  sudo -u oracle -E $STAGE/database/runInstaller -silent -showProgress -promptForPassword -waitforcompletion -responseFile $BASEDIR/db_install.rsp
+fi
 
 # Run root scripts to complete the database software install
-
-su - root -c '/u01/app/oraInventory/orainstRoot.sh'
-su - root -c '/u01/app/oracle/product/12c/root.sh'
-
+/u01/app/oraInventory/orainstRoot.sh
+[ -f $ORACLE_HOME/root.sh ] && bash $ORACLE_HOME/root.sh
 
 # Run post install scripts which includes the option to create a database
 # Referred to Oracle DB Install Guide - A.5,6 Running Database Configuration Assistant Using a Response File
 # http://docs.oracle.com/database/121/LADBI/app_nonint.htm#LADBI7843
-
 if [ `cat /proc/meminfo | grep MemTotal | awk '{print $2}'` -gt 1000000 ]; then
     [ `ps -ef | grep pmon | grep oracle | wc -l` -gt 0 ] || su - oracle -c '/u01/app/oracle/product/12c/cfgtoollogs/configToolAllCommands RESPONSE_FILE=/vagrant/cfgrsp.properties' 
   else
